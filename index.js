457c40901f7273cbd9ae63373dea663af9569a7d
Jonathan Clem <jonathan@jclem.net>
2014-03-24 16:12:31 -0700

    log url on every line

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var concat = require('concat-stream');
 var logfmt = require('logfmt');
 
 require('http').createServer(function(req, res) {
-  var logger = logfmt.namespace({ ns: 'http-sponge', request_id: req.headers['x-request-id'] });
+  var logger = logfmt.namespace({ ns: 'http-sponge', request_id: req.headers['x-request-id'], url: req.url });
 
   req.pipe(concat(logRequest));
   req.on('end', end);
@@ -10,7 +10,7 @@ require('http').createServer(function(req, res) {
   function logRequest(data) {
     data = data.length ? data : '{}';
 
-    logger.log({ at: 'metadata', url: req.url, method: req.method });
+    logger.log({ at: 'metadata', method: req.method });
     logger.log(extend({ at: 'headers' }, req.headers));
     logger.log(extend({ at: 'body-parsed' }, JSON.parse(data)));
     logger.log({ at: 'body-json', body: data });

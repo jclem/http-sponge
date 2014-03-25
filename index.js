ab5e9a19ec97c6d074d4482c15cb1e38598a1737
Jonathan Clem <jonathan@jclem.net>
2014-03-25 11:17:20 -0700

    simplify pipes

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ var logfmt = require('logfmt');
 require('http').createServer(function(req, res) {
   var logger = logfmt.namespace({ ns: 'http-sponge', request_id: req.headers['x-request-id'], url: req.url });
 
-  req.pipe(concat(logRequest));
-  req.on('end', end);
-
-  function logRequest(data) {
+  req.pipe(concat(function(data) {
     var logData = {
       ns: 'http-sponge',
       url: req.url,
@@ -18,17 +15,9 @@ require('http').createServer(function(req, res) {
     logData.body    = data.length ? data : '{}';
 
     logger.log(logData);
-  }
+  }));
 
-  function end() {
+  req.on('end', function() {
     res.end('ok');
-  }
-
-  function extend(target, source) {
-    for (var key in source) {
-      target[key] = source[key];
-    }
-
-    return target;
-  }
+  });
 }).listen(process.env.PORT || 5000);
